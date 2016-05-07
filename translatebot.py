1ebe2567b75a5908b20e99772414f755039a7a22
milkmandan <ben@intechnicolor.net>
2016-05-07 03:40:19 -0700

    Added duplex mode and optimizations

--- translatebot.py
+++ translatebot.py
@@ -1,10 +1,7 @@
 # Twitch Translation Chat Bot
-# A chat bot written in Python3 that automatically will translate your Twitch chat language for your foriegn viewers.
+# A chat bot written in Python that automatically will translate your Twitch chat language for your foriegn viewers.
 # 2016 - Benjamin Chu - v0.1
 
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 import socket # imports module allowing connection to IRC
 import threading # imports module allowing timing functions
 import requests # imports module allowing requests
@@ -15,33 +12,26 @@ import ConfigParser # imports module allowing reading of .ini files
 import os # for relative pathing
 import string # for string manipulation
 import datetime
-
-# https://translate.yandex.net/api/v1.5/tr.json/detect ? 
-# key=<API key>
-#  & text=<text>
-#  & [callback=<name of the callback function>]
-
-# https://translate.yandex.net/api/v1.5/tr.json/translate ? 
-# key=<API key>
-#  & text=<text to translate>
-#  & lang=<translation direction>
-#  & [format=<text format>]
-#  & [options=<translation options>]
-#  & [callback=<name of the callback function>]
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 # =============================================
 # FEATURE LIST - Add
 # - Ability for users to change target language in chat
 # - Static settings file
-# - Commands
-# - Broadcast to all function and then whisper only function
+# - Whisper mode
 # # =============================================
 # BUG LIST
-# - quick successive message cause index error. need to find a way to parse out the ircdata
+# - Quick successive message cause index error. Need to find a way to parse out the ircdata properly
 
 class APIROUTES:
-	detect = 'https://translate.yandex.net/api/v1.5/tr.json/detect?key={key}&text={input}'
-	translate = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key={key}&text={input}&lang={target}'
+	yandex_detect = 'https://translate.yandex.net/api/v1.5/tr.json/detect?key={key}&text={input}'
+	yandex_translate = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key={key}&text={input}&lang={target}'
+	google_detect = 'https://www.googleapis.com/language/translate/v2/detect?key={key}&q={input}'
+	google_translate = 'https://www.googleapis.com/language/translate/v2?key={key}&q={input}&source={source}&target={target}'
+	#google_auto_translate = 'https://www.googleapis.com/language/translate/v2?key={key}&q={input}&target={target}'
+
 
 # ====== READ CONFIG ======
 Config = ConfigParser.ConfigParser()
@@ -65,18 +55,20 @@ def ConfigSectionMap(section):
 # Set variables for connection
 botOwner = ConfigSectionMap('settings')['botowner']
 nick = ConfigSectionMap('settings')['nick']
-channel = '#' + ConfigSectionMap('settings')['channel']
+channel = '#darkandark'
+# channel = '#' + ConfigSectionMap('settings')['channel']
 server = ConfigSectionMap('settings')['server']
 port = int(ConfigSectionMap('settings')['port'])
 password = ConfigSectionMap('settings')['oauth']
-api_key = ConfigSectionMap('settings')['api']
+yandex_api_key = ConfigSectionMap('settings')['yandexapi']
+google_api_key = ConfigSectionMap('settings')['googleapi']
 
 # Translation Bot Default Settings
-targetLanguage = 'ja'
-filterLanguage = 'en'
-broadcast = 0 # 1 for ON | 0 for OFF
-
-
+targetLanguage = 'ja' # translating TO
+sourceLanguage = 'en' # translating FROM
+duplex = 0 # 1 for ON | 0 for OFF
+broadcast = 1 # 1 for ON | 0 for OFF
+translateAPI = 'yandex' # google | yandex
 
 # ====== IRC FUNCTIONS ======
 # Extract Nickname
@@ -93,35 +85,80 @@ def getMessage(data):
 			message = data.split(channel, 1)[1][2:]
 			return message
 		except IndexError:
-			return 'Index Error'
+			return 'Message Error'
 		except:
-			return 'No message'
+			return 'Message Error'
 	else:
-		return 'Not a message'
+		return 'Message Error'
 
 def detectLanguage(data):
-	tempDict = requests.get(APIROUTES.detect.format(key=api_key, input=data)).json()
-	try:
-		language = tempDict['lang']
-		return str(language)
-	except KeyError:
-		print 'Fails gracefully, problem with detection'
-		return 'Nothing'
+	if translateAPI == 'yandex':
+		# Get json data put into a dictionary
+		tempDict = requests.get(APIROUTES.yandex_detect.format(key=yandex_api_key, input=data)).json()
+		try:
+			language = tempDict['lang']
+			return str(language)
+		except KeyError:
+			print 'Problem with detection.'
+			return 'Could not detect language.'
+	elif translateAPI == 'google':
+		tempDict = requests.get(APIROUTES.google_detect.format(key=google_api_key, input=data)).json()
+		try:
+			language = tempDict['data']['detections'][0][0]['language']
+			#isReliable = tempDict['data']['detections'][0][0]['isReliable']
+			#confidence = tempDict['data']['detections'][0][0]['confidence']
+			return str(language)
+		except KeyError:
+			print 'Problem with detection.'
+			return 'Could not detect language.'
+	# Defaults to google's API
+	else:
+		tempDict = requests.get(APIROUTES.google_detect.format(key=google_api_key, input=data)).json()
+		try:
+			language = tempDict['data']['detections'][0][0]['language']
+			#isReliable = tempDict['data']['detections'][0][0]['isReliable']
+			#confidence = tempDict['data']['detections'][0][0]['confidence']
+			return str(language)
+		except KeyError:
+			print 'Problem with detection.'
+			return 'Could not detect language.'
 
+def translateMessage(data, sourceLanguage, targetLanguage):
+	print 'Translating: ' + data
+	if translateAPI == 'yandex':
+		tempDict = requests.get(APIROUTES.yandex_translate.format(key=yandex_api_key, input=data, target=str(targetLanguage))).json()
+		try:
+			translatedText = tempDict['text'][0]
+			print 'Result: ' + translatedText
+			return translatedText
+		except KeyError:
+			print 'Ignoring Debug Data'
+			return 'Input Error'
+	elif translateAPI == 'google':
+		tempDict = requests.get(APIROUTES.google_translate.format(key=google_api_key, input=data, source=str(sourceLanguage), target=str(targetLanguage))).json()
+		try:
+			translatedText = tempDict['data']['translations'][0]['translatedText']
+			print 'Result: ' + translatedText
+			return translatedText
+		except KeyError:
+			print 'Ignoring Debug Data'
+			return 'Input Error'
+	# Defaults to Google API
+	else:
+		tempDict = requests.get(APIROUTES.google_translate.format(key=google_api_key, input=data, source=str(sourceLanguage), target=str(targetLanguage))).json()
+		try:
+			translatedText = tempDict['data']['translations'][0]['translatedText']
+			print 'Result: ' + translatedText
+			return translatedText
+		except KeyError:
+			print 'Ignoring Debug Data'
+			return 'Input Error'
 
-def translateMessage(data):
-	tempDict = requests.get(APIROUTES.translate.format(key=api_key, input=data, target=str(targetLanguage))).json()
-	try:
-		translatedText = tempDict['text'][0]
-		return translatedText
-	except KeyError:
-		print 'Ignoring Debug Data'
-		return 'There was a problem with the input'
 
 # ====== TIMER FUNCTIONS ======
 
-def printit():  
-	threading.Timer(25.0, printit).start()
+def keepAlive():  
+	threading.Timer(60.0, keepAlive).start()
 
 	print 'Keep Alive'
 
@@ -139,9 +176,11 @@ irc.send('USER ' + nick + ' 0 * :' + botOwner + '\r\n')
 irc.send('NICK ' + nick + '\r\n')
 irc.send('JOIN ' + channel + '\r\n')
 
-printit()
-
-irc.send('PRIVMSG ' + channel + ' :/me has entered the channel.\r\n')
+keepAlive()
+currentSettings = 'Current Settings: [' + sourceLanguage.upper() + '<->' + targetLanguage.upper()\
+			+'] [Using ' + translateAPI.title() + ' Translate API] [Broadcast ' + str(broadcast)\
+			+'] [Duplex ' + str(duplex) + ']'   
+irc.send('PRIVMSG ' + channel + ' :/me has entered the channel. ' + currentSettings + ' \r\n')
 
 # translation_bot has entered the channel. Settings
 # [ Target Language: jp | Filter Language: None | Broadcast: ON | !botcommands  | !about ]
@@ -154,16 +193,12 @@ while True:
 	ircuser = ircdata.split(':')[1]
 	ircuser = ircuser.split('!')[0] # determines the sender of the messages
 
-	print 'DEBUG: ' + ircdata.strip(' \t\n\r')
-	print '\n'
+	print 'DEBUG:\n' + ircdata.strip(' \t\n\r') + '\n'
 	user = getNick(ircdata).strip(' \t\n\r')
-	print 'USER: ' + user
-	print '\n'
+	print 'USER:\n' + user + '\n'
 	message = getMessage(ircdata).strip(' \t\n\r')
-	print 'MESSAGE: ' + message
-	print '\n'
+	print 'MESSAGE:\n' + message
 	print '======================='
-
 
 	# About
 	if ircdata.find(':!about') != -1:
@@ -173,40 +208,75 @@ while True:
 	if ircdata.find(':!why') != -1:
 		irc.send('PRIVMSG ' + channel + ' :Twitch Chat is a wonderful platform to connect gamers and build communities. Many streamers have a dual language audience. Having a translation bot in your channel may help viewers of different languages interact, increasing the quality of your chat for your viewers.\r\n')
 
-	# Last
-	if ircdata.find(':!last') != -1:
-		irc.send('PRIVMSG ' + channel + ' :' + getLast() + '\r\n')
-
-	# Last
+	# !broadcast
 	if ircdata.find(':!broadcast') != -1:
 		tempMsg = message.strip('!broadcast')
-		tempMsg = tempMsg.split()[0]
-		if tempMsg.lower() == 'on':
-			broadcast = 1;
-		elif tempMsg.lower() == 'off':
-			broadcast = 0;
-		else:
-			pass
-			
+		try:
+			tempMsg = tempMsg.split()[0]
+			if tempMsg.lower() == 'on' or tempMsg == '1':
+				irc.send('PRIVMSG ' + channel + ' :Broadcast set to ON.\r\n')	
+				broadcast = 1;
+			elif tempMsg.lower() == 'off' or tempMsg == '0':
+				irc.send('PRIVMSG ' + channel + ' :Broadcast set to OFF.\r\n')
+				broadcast = 0;
+			else:
+				irc.send('PRIVMSG ' + channel + ' :Broadcast not set!\r\n')
+				print 'Broadcast Not Set'
+		except IndexError:
+			print 'Broadcast Not Set'
 
+	# !duplex | Bi-Directional Translation
+	if ircdata.find(':!duplex') != -1:
+		tempMsg = message.strip('!duplex')
+		try:
+			tempMsg = tempMsg.split()[0]
+			if tempMsg.lower() == 'on' or tempMsg == '1':
+				irc.send('PRIVMSG ' + channel + ' :Duplex set to ON.\r\n')
+				duplex = 1;
+			elif tempMsg.lower() == 'off' or tempMsg == '0':
+				irc.send('PRIVMSG ' + channel + ' :Duplex set to OFF.\r\n')
+				duplex = 0;
+			else:
+				irc.send('PRIVMSG ' + channel + ' :Duplex not set!\r\n')
+				print 'Duplex Not Set'
+		except IndexError:
+			print 'Duplex Not Set'
+			
 	# Current Runes
 	if ircdata.find(':!settings') != -1 or ircdata.find(':!setting') != -1:
-		irc.send('PRIVMSG ' + channel + ' :' + getCurrent('runes') + '\r\n')
+		print 'duplex ' + str(duplex)
+		print 'sourceLang ' + str(sourceLanguage)
+		print 'targetLang ' + str(targetLanguage)
+		print 'Broadcast ' + str(broadcast)
+
+		currentSettings = 'Current Settings: [' + sourceLanguage.upper() + '<->' + targetLanguage.upper()\
+			+'] [Using ' + translateAPI.title() + ' Translate API] [Broadcast ' + str(broadcast)\
+			+'] [Duplex ' + str(duplex) + ']'   
+		irc.send('PRIVMSG ' + channel + ' :' + currentSettings + '\r\n')
+		
+
+		# Translation Bot Default Settings
+# targetLanguage = 'ja' # translating TO
+# sourceLanguage = 'en' # translating FROM
+# duplex = 0 # 1 for ON | 0 for OFF
+# broadcast = 1 # 1 for ON | 0 for OFF
+# translateAPI = 'google' # google | yandex
 
 	# Current Mastery
-	if ircdata.find(':!botcommands') != -1 or ircdata.find(':!botcommand') != -1:
-		irc.send('PRIVMSG ' + channel + ' :' + getCurrent('masteries') + '\r\n')
+	#if ircdata.find(':!botcommands') != -1 or ircdata.find(':!botcommand') != -1:
+		#irc.send('PRIVMSG ' + channel + ' :' + getCurrent('masteries') + '\r\n')
 
 	# Keep Alive
 	if ircdata.find('PING') != -1:
 		irc.send('PONG ' + ircdata.split()[1] + '\r\n')
 
 	inputText = message
-	if inputText == 'Not a message' or inputText == 'No message' or inputText == 'Index Error' or inputText[0] == '!':
+	if inputText == 'Message Error' or inputText[0] == '!':
 		pass
-	elif detectLanguage(str(inputText)) == str(filterLanguage) and broadcast == 1:
-		print 'IS IT PICKING UP THAT IT IS ENGLIHS?'
-		irc.send('PRIVMSG ' + channel + ' :/me : ' + user + ' said - [' + translateMessage(str(inputText)) + '] ' + '\r\n')
+	elif detectLanguage(str(inputText)) == str(sourceLanguage) and broadcast == 1:
+		irc.send('PRIVMSG ' + channel + ' :/me : ' + user + ' > [' + translateMessage(str(inputText), sourceLanguage, targetLanguage) + '] ' + '\r\n')
+	elif duplex == 1:
+		irc.send('PRIVMSG ' + channel + ' :/me : ' + user + ' > [' + translateMessage(str(inputText), targetLanguage, sourceLanguage) + '] ' + '\r\n')
 	else:
-		print filterLanguage + ' not found.'
+		print 'Broadcast Disabled / Bi-Direction Disabled / Source Language Not Found'
 		pass
